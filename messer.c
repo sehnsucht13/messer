20339b5b86589ce76cda41ca744e6973f99f41a8
Yavor Konstantinov <ykonstantinov1@gmail.com>
2019-02-19 22:03:46 -0800

    Implemented desktop structures and cleaned up code

--- messer.c
+++ messer.c
@@ -1,6 +1,7 @@
 #include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h>
 #include <xcb/xcb.h>
 /*   
 Command to start up zephyr for testing
@@ -10,37 +11,74 @@ xcb_connection_t *connection;
 xcb_screen_t *screen;
 bool exitFlag = true;
 xcb_generic_event_t *e;
-xcb_window_t window;
+uint16_t rootWidth, rootHeight;
 uint32_t maskWindow;
-uint32_t mask[1] = { XCB_EVENT_MASK_SUBSTRUCTURE_NOTIFY |
-					 XCB_EVENT_MASK_SUBSTRUCTURE_REDIRECT};
+int screen_default_nbr;
+uint32_t rootMask[1] = { XCB_EVENT_MASK_SUBSTRUCTURE_NOTIFY |
+					 XCB_EVENT_MASK_SUBSTRUCTURE_REDIRECT | XCB_EVENT_MASK_PROPERTY_CHANGE | XCB_EVENT_MASK_BUTTON_PRESS};
 uint32_t values[2];
-/* uint32_t mask[1] = { */
-/*         XCB_EVENT_MASK_SUBSTRUCTURE_NOTIFY | // destroy notify */
-/*         XCB_EVENT_MASK_SUBSTRUCTURE_REDIRECT }; // map request */
-/* xcb_change_window_attributes(conn, screen->root, XCB_CW_EVENT_MASK, mask); */
 
-void handleKeypress(xcb_key_press_event_t *event) {
-  printf("Keyboard event."); }
+const uint16_t reconWinMask = XCB_CONFIG_WINDOW_X | XCB_CONFIG_WINDOW_Y | XCB_CONFIG_WINDOW_WIDTH | XCB_CONFIG_WINDOW_HEIGHT;
+
+struct workspaceWindow {
+  struct workspaceWindow *nextWindow;
+  xcb_window_t currWindow;
+};
+
+struct workspace {
+  struct workspaceWindow *nextWindow;
+  int numOfWindows;
+};
+
+struct workspace *Workspace;
+/* Create a workspace */
+void initWorkspace() {
+  Workspace = malloc(sizeof(struct workspace));
+  // Initialize
+  Workspace->nextWindow = NULL;
+  Workspace->numOfWindows = 0;
+}
+
+void mapRequest(xcb_map_request_event_t *mapEvent) {
+  xcb_window_t newWindow = mapEvent->window;
+  /* xcb_get_geometry_cookie_t replyCookie = xcb_get_geometry(connection, newWindow); */
+  /* xcb_get_geometry_reply_t *geometryReply = xcb_get_geometry_reply(connection, replyCookie, NULL); */
+  /* geometryReply-> */
+  if (Workspace->numOfWindows == 0) {
+    uint32_t reconValues[4] = { 0,0,rootWidth, rootHeight};
+	xcb_configure_window(connection, newWindow, reconWinMask, reconValues);
+	xcb_flush(connection);
+  }
+  struct workspaceWindow *window = malloc(sizeof(struct workspaceWindow));
+  Workspace->nextWindow = window;
+  Workspace->numOfWindows += 1;
+}
 
 int main(int argc, char *argv[]) {
-  connection = xcb_connect(NULL, NULL);
   // Get the current screen
   screen = xcb_setup_roots_iterator(xcb_get_setup(connection)).data;
-  xcb_change_window_attributes(connection, screen->root, XCB_CW_EVENT_MASK, mask);
-  window = xcb_generate_id(connection);
-  maskWindow = XCB_CW_BACK_PIXEL | XCB_CW_EVENT_MASK;
-  values[0] = screen->white_pixel;
-  values[1] = XCB_EVENT_MASK_EXPOSURE | XCB_EVENT_MASK_BUTTON_PRESS |
-              XCB_EVENT_MASK_BUTTON_RELEASE | XCB_EVENT_MASK_POINTER_MOTION |
-              XCB_EVENT_MASK_ENTER_WINDOW | XCB_EVENT_MASK_LEAVE_WINDOW |
-              XCB_EVENT_MASK_KEY_PRESS | XCB_EVENT_MASK_KEY_RELEASE;
+  // Make the root window detect events which modify
+  xcb_change_window_attributes(connection, screen->root, XCB_CW_EVENT_MASK, rootMask);
 
-  xcb_create_window(connection, 0, window, screen->root, 400, 400, 100, 100, 10,
-                    XCB_WINDOW_CLASS_INPUT_OUTPUT, screen->root_visual, maskWindow,
-                    values);
-  xcb_map_window(connection, window);
-  xcb_flush(connection);
+  // Width and height of the current window
+  rootWidth = screen->width_in_pixels;
+  rootHeight = screen->height_in_pixels;
+
+  // Create a test window to detect events
+  /* window = xcb_generate_id(connection); */
+  /* maskWindow = XCB_CW_BACK_PIXEL | XCB_CW_EVENT_MASK; */
+  /* values[0] = screen->white_pixel; */
+  /* values[1] = XCB_EVENT_MASK_EXPOSURE | XCB_EVENT_MASK_BUTTON_PRESS | */
+  /*             XCB_EVENT_MASK_BUTTON_RELEASE | XCB_EVENT_MASK_POINTER_MOTION | */
+  /*             XCB_EVENT_MASK_ENTER_WINDOW | XCB_EVENT_MASK_LEAVE_WINDOW | */
+  /*             XCB_EVENT_MASK_KEY_PRESS | XCB_EVENT_MASK_KEY_RELEASE; */
+
+  /* xcb_create_window(connection, 0, window, screen->root, 550, 200, 150, 300, 10, */
+  /*                   XCB_WINDOW_CLASS_INPUT_OUTPUT, screen->root_visual, maskWindow, */
+  /*                   values); */
+  /* xcb_map_window(connection, window); */
+  /* xcb_flush(connection); */
+
   // Main loop
   // Either catch an event or check if the program must shutdown
   while ((e = xcb_wait_for_event(connection)) && exitFlag) {
@@ -63,18 +101,18 @@ int main(int argc, char *argv[]) {
 	  printf("Configure request\n");
 	  break;
     case XCB_MAP_REQUEST:
-      printf("Map request caught\n");
+	  // This even is the first one called when a new window wants to be created
+      printf("Map request Event caught\n");
+	  xcb_map_request_event_t *mapRequestEvent = (xcb_map_request_event_t *)e;
+	  mapRequest(mapRequestEvent);
     case XCB_EXPOSE:
-      /* xcb_expose_event_t *expEv = (xcb_expose_event_t *)e; */
 	  // Third event
       printf("Caught an expose event!\n");
       break;
     case XCB_KEY_PRESS: {
-      xcb_key_press_event_t *keyEv = (xcb_key_press_event_t *)e;
-      handleKeypress(keyEv);
+	  printf("Key press detected\n");
       break;
     }
-
     default:
       printf("not handled\n");
     }
